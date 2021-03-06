cca7aeac2d187408f49ec2946f393a00dbb5dde1
金圣聪 <jinshengcong@Gmail.com>
2021-03-07 00:42:55 +0800

    改善窗口关闭监听

--- src/com/jinshengcong/tank/TankFrame.java
+++ src/com/jinshengcong/tank/TankFrame.java
@@ -24,42 +24,49 @@ public class TankFrame extends Frame {
         this.addKeyListener(new MyKeyListener() {
 
         });
-        this.addWindowListener(new WindowListener() {
-            @Override
-            public void windowOpened(WindowEvent e) {
-
-            }
-
+        this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
             }
-
-            @Override
-            public void windowClosed(WindowEvent e) {
-
-            }
-
-            @Override
-            public void windowIconified(WindowEvent e) {
-
-            }
-
-            @Override
-            public void windowDeiconified(WindowEvent e) {
-
-            }
-
-            @Override
-            public void windowActivated(WindowEvent e) {
-
-            }
-
-            @Override
-            public void windowDeactivated(WindowEvent e) {
-
-            }
         });
+        // new WindowListener() {
+        //     @Override
+        //     public void windowOpened(WindowEvent e) {
+        //
+        //     }
+        //
+        //     @Override
+        //     public void windowClosing(WindowEvent e) {
+        //         System.exit(0);
+        //     }
+        //
+        //     @Override
+        //     public void windowClosed(WindowEvent e) {
+        //
+        //     }
+        //
+        //     @Override
+        //     public void windowIconified(WindowEvent e) {
+        //
+        //     }
+        //
+        //     @Override
+        //     public void windowDeiconified(WindowEvent e) {
+        //
+        //     }
+        //
+        //     @Override
+        //     public void windowActivated(WindowEvent e) {
+        //
+        //     }
+        //
+        //     @Override
+        //     public void windowDeactivated(WindowEvent e) {
+        //
+        //     }
+        // }
+
     }
 
     // 闪烁的解决 双缓冲
